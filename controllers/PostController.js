4ea1fab500cc5f057b63164ac71053aaa2f960d3
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-08-13 13:54:32 +0300

    test

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,68 +1,72 @@
-const { Post } = require('../models')
-require('dotenv').config()
+const { Post } = require("../models");
+require("dotenv").config();
 
 const ReadPost = async (req, res) => {
   try {
     // let verifyToken = await middleware.verifyToken()
-    const posts = await Post.find({})
-    res.send(posts)
+    const posts = await Post.find({}).populate("owner");
+    res.send(posts);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 /////////// only the user can view his posts ///////////
 const GetPostsByUser = async (req, res) => {
   try {
-    const { user_id } = req.params
-    const posts = await Post.find({ owner: user_id })
-    res.send(posts)
+    const { user_id } = req.params.user_id;
+    const posts = await Post.find({ owner: user_id });
+    res.send(posts);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 /////////// only the user can view his posts ///////////
 
 const CreatePost = async (req, res) => {
   try {
-    const { title, description, owner } = req.body
-    const img = req.file.filename
+    const { title, description, owner } = req.body;
+    const img = req.file.filename;
     const post = await Post.create({
       title,
       description,
       img,
-      owner
-    })
-    res.send(post)
+      owner,
+    });
+    res.send(post);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 const UpdatePost = async (req, res) => {
   try {
     const post = await Post.findByIdAndUpdate(req.params.post_id, req.body, {
-      new: true
-    })
-    res.send(post)
+      new: true,
+    });
+    res.send(post);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 const DeletePost = async (req, res) => {
   try {
-    await Post.deleteOne({ _id: req.params.post_id })
-    res.send({ msg: 'Post Deleted', payload: req.params.post_id, status: 'Ok' })
+    await Post.deleteOne({ _id: req.params.post_id });
+    res.send({
+      msg: "Post Deleted",
+      payload: req.params.post_id,
+      status: "Ok",
+    });
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 module.exports = {
   ReadPost,
   CreatePost,
   UpdatePost,
   DeletePost,
-  GetPostsByUser
-}
+  GetPostsByUser,
+};
